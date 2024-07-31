c07263d3eadb49961ca579cb41efb1575b17ea39
Amreen Kaur <amreen.kaur@gmail.com>
2024-07-30 22:39:19 -0600

    finished website yay kind of

--- Personal Portfolio/script.js
+++ Personal Portfolio/script.js
@@ -14,3 +14,19 @@ function opentab(tabname)
     event.currentTarget.classList.add("active-link");
     document.getElementById(tabname).classList.add("active-tab");
 }
+
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwwQ4Nu9vFKcI0LUZWsxXvP-ksYCKQmdqougimPBe_df_dl1RPtim5L4FOGcAHs6AU/exec'
+  const form = document.forms['submit-to-google-sheet']
+  const msg = document.getElementById("msg")
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+      .then(response => {
+        msg.innerHTML = "Message sent successfully"
+        setTimeout(function(){
+            msg.innerHTML = ""
+        }, 5000)
+        form.reset()
+      })
+      .catch(error => console.error('Error!', error.message))
+  })
